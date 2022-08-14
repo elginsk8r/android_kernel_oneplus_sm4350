808aed1e57934e0e58b8a10cc05c85441b44e014
Albert Tang <tangalbert919@yahoo.com>
2022-08-13 21:42:30 -0500

    treewide: Import minimal changes from DE2117_11_C.15
    Change-Id: I5da6038f62ba6e9fe69945a27c32f3c598708f63

--- drivers/soc/qcom/icnss2/qmi.c
+++ drivers/soc/qcom/icnss2/qmi.c
@@ -31,6 +31,10 @@
 #include "debug.h"
 #include "genl.h"
 
+#if IS_ENABLED(CONFIG_CNSS_UTILS)
+#include <net/cnss_utils.h>
+#endif
+
 #define WLFW_SERVICE_WCN_INS_ID_V01	3
 #define WLFW_SERVICE_INS_ID_V01		0
 #define WLFW_CLIENT_ID			0x4b4e454c
@@ -801,6 +805,10 @@ int icnss_wlfw_wlan_mac_req_send_sync(struct icnss_priv *priv,
 	struct wlfw_mac_addr_resp_msg_v01 resp = {0};
 	struct qmi_txn txn;
 	int ret;
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_WIFI_MAC)
+	int i;
+	char revert_mac[QMI_WLFW_MAC_ADDR_SIZE_V01];
+#endif
 
 	if (!priv || !mac || mac_len != QMI_WLFW_MAC_ADDR_SIZE_V01)
 		return -EINVAL;
@@ -816,8 +824,24 @@ int icnss_wlfw_wlan_mac_req_send_sync(struct icnss_priv *priv,
 
 	icnss_pr_dbg("Sending WLAN mac req [%pM], state: 0x%lx\n",
 			     mac, priv->state);
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_WIFI_MAC)
+	for (i = 0; i < QMI_WLFW_MAC_ADDR_SIZE_V01; i++) {
+		revert_mac[i] = mac[QMI_WLFW_MAC_ADDR_SIZE_V01 - i - 1];
+	}
+	icnss_pr_dbg("Sending revert WLAN MAC request [%pM], state: 0x%lx\n",
+		    revert_mac, priv->state);
+	memcpy(req.mac_addr, revert_mac, mac_len);
+#else
 	memcpy(req.mac_addr, mac, mac_len);
+#endif
 	req.mac_addr_valid = 1;
+
+#if IS_ENABLED(CONFIG_CNSS_UTILS)
+	ret = cnss_utils_set_wlan_mac_address(req.mac_addr, mac_len);
+	if (ret < 0) {
+		icnss_pr_err("Failed to set cnss utils wlan mac address (non-fatal), err: %d\n", ret);
+	}
+#endif
 
 	ret = qmi_send_request(&priv->qmi, NULL, &txn,
 			       QMI_WLFW_MAC_ADDR_REQ_V01,
